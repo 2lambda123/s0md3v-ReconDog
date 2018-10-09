3e9f57fb77c95f41fcc4d0c8d47ce972524b7354
Somdev Sangwan <s0md3v@gmail.com>
2018-10-09 13:49:16 +0530

    Add files via upload

---
+++ plugins/nsLookup.py
@@ -0,0 +1,6 @@
+import sys
+from requests import get
+
+def nsLookup(inp):
+    result = get('http://api.hackertarget.com/dnslookup/?q=' + inp).text
+    sys.stdout.write(result)
---
+++ plugins/whois.py
@@ -0,0 +1,6 @@
+import sys
+from requests import get
+
+def whois(inp):
+    result = get('http://api.hackertarget.com/whois/?q=' + inp).text
+    sys.stdout.write(result)
